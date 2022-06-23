7fcd41e3b0e141f855a2ab7595ae36255263e394
siobhan <siobhan.boyle@domino-uk.com>
2022-06-23 22:50:36 +0100

    let's go kubes

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 import pulumi
 import pulumi_docker as docker
+import pulumi_kubernetes as k8s
 
 # Fetch the Docker Hub auth info from config.
 config = pulumi.Config()
@@ -27,3 +28,31 @@ image = docker.Image('my-image',
 # Export the base and specific version image name.
 pulumi.export('baseImageName', image.base_image_name)
 pulumi.export('fullImageName', image.image_name)
+
+
+# Create a load balanced Kubernetes service using this image, and export its IP.
+app_labels = { 'app': 'myapp' }
+app_dep = k8s.apps.v1.Deployment('app-dep',
+    spec={
+        'selector': { 'matchLabels': app_labels },
+        'replicas': 3,
+        'template': {
+            'metadata': { 'labels': app_labels },
+            'spec': {
+                'containers': [{
+                    'name': 'myapp',
+                    'image': image.image_name,
+                }],
+            },
+        },
+    },
+)
+app_svc = k8s.core.v1.Service('app-svc',
+    metadata={ 'labels': app_labels },
+    spec={
+        'type': 'LoadBalancer',
+        'ports': [{ 'port': 80, 'targetPort': 80, 'protocol': 'TCP' }],
+        'selector': app_labels,
+    }
+)
+pulumi.export('appIp', app_svc.status.apply(lambda s: s.loadbalancer.ingress[0].ip))
